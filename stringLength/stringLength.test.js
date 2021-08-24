5b322eba61290b779bd171aca49acde7185f09be
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-08-24 11:14:29 +0100

    Add test for string length greater than 10

--- stringLength/stringLength.test.js
+++ stringLength/stringLength.test.js
@@ -4,7 +4,10 @@ test('returns the length of a string', () => {
   expect(stringLength('soring')).toBe(6)
 })
 
-test('throws error when length is less than 1', () => {
+test('throws error when string length is less than 1', () => {
   expect(() => stringLength('')).toThrow('Less than 1 or greater than 10')
 })
 
+test('throws error when string length is greater then 10', () => {
+  expect(() => stringLength('greater than 10')).toThrow('Less than 1 or greater than 10')
+})
